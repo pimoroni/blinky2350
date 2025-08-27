8e8f1ff45837abb78873a5db6e0fe0f4c645235f
Phil Howard <github@gadgetoid.com>
2025-08-27 12:20:25 +0100

    Display MSC message on screen.

--- modules/common/_boot_fat.py
+++ modules/common/_boot_fat.py
@@ -21,6 +21,15 @@ except:  # noqa: E722
 
 
 if powman.get_wake_reason() == powman.WAKE_DOUBLETAP:
+    import blinky
+    from picographics import PicoGraphics
+    blinky = blinky.Blinky()
+    display = PicoGraphics(width=blinky.WIDTH, height=blinky.HEIGHT)
+    display.set_pen(0)
+    display.clear()
+    display.set_pen(0x22)
+    display.text("USB\nDisk\nMode", 1, 0, scale=1)
+    blinky.update(display)
     rp2.enable_msc()
 
 del os, vfs, bdev
