e1d0654142df37af3cc65ded224e49c687577962
thirdr <ryan@pimoroni.com>
2026-01-21 11:22:59 +0000

    apps: initial commit for menu app

--- firmware/apps/menu/__init__.py
+++ firmware/apps/menu/__init__.py
@@ -1,96 +1,38 @@
-import sys
 import os
+import sys
 
 sys.path.insert(0, "/system/apps/menu")
+sys.path.insert(0, "/")
 os.chdir("/system/apps/menu")
 
-import math
-from badgeware import is_dir, file_exists, run, set_brightness
-from icon import Icon
-import ui
+from badgeware import run, set_brightness
+from app import Apps
 
 set_brightness(0.2)
+screen.font = pixel_font.load("/system/assets/fonts/ark.ppf")
 
-# define the list of installed apps
-#
-# - hack them!
-# - replace them with your own
-# - reorder them
-# - what would mona do...?
-apps = [
-    ("mona's quest", "quest"),
-    ("mona pet", "monapet"),
-    ("monasketch", "sketch"),
-    ("flappy mona", "flappy"),
-    ("gallery", "gallery"),
-    ("badge", "badge"),
-]
-
-mona = SpriteSheet("/system/assets/mona-sprites/mona-default.png", 11, 1)
-screen.font = rom_font.ark
-# screen.antialias = image.X2
-
-cx, cy = int(screen.width / 2), int(screen.height / 2)
-
-# find installed apps and create icons
-icons = []
-for app in apps:
-    name, path = app[0], app[1]
-
-    if is_dir(f"/system/apps/{path}"):
-        if file_exists(f"/system/apps/{path}/icon.png"):
-            x = cx + 1
-            y = cy
-            pos = (x, y)
-            sprite = image.load(f"/system/apps/{path}/icon.png")
-            icons.append(Icon(pos, name, len(icons), sprite))
-
-active = 0
-
-MAX_ALPHA = 255
-alpha = 30
+# find installed apps and create apps
+apps = Apps("/system/apps")
 
 
 def update():
-    global active, icons, alpha
 
-    # process button inputs to switch between icons
+    screen.pen = color.rgb(0, 0, 0)
+    screen.clear()
+
+    # process button inputs to switch between apps
     if io.BUTTON_C in io.pressed:
-        active += 1
+        apps.next()
+        print(apps.active.name)
     if io.BUTTON_A in io.pressed:
-        active -= 1
-#     if io.BUTTON_UP in io.pressed:
-#         active -= 3
-#     if io.BUTTON_DOWN in io.pressed:
-#         active += 3
+        apps.prev()
+        print(apps.active.name)
+
     if io.BUTTON_B in io.pressed:
-        return f"/system/apps/{apps[active][1]}"
-    active %= len(icons)
+        apps.launch()
 
-    ui.draw_background()
-    ui.draw_header()
-
-    # draw menu icons
-    for i in range(len(icons)):
-        icons[i].activate(active == i)
-#         icons[i].draw()
-
-    # draw label for active menu icon
-    if Icon.active_icon:
-        icons[active].draw()
-        label = f"{Icon.active_icon.name}"
-        w, _ = screen.measure_text(label)
-        screen.pen = color.rgb(211, 250, 55)
-#         screen.shape(shape.rounded_rectangle(cx - (w / 2) - 4, screen.height - 20, w + 8, 15, 4))
-        screen.pen = color.rgb(0, 0, 0, 150)
-#         screen.text(label, 0, 0)
-
-    if alpha <= MAX_ALPHA:
-        screen.pen = color.rgb(0, 0, 0, 255 - alpha)
-        screen.clear()
-        alpha += 30
-
-    return None
+    # draw menu apps
+    return apps.draw()
 
 
 if __name__ == "__main__":
---
+++ firmware/apps/menu/app.py
@@ -0,0 +1,108 @@
+import os
+from badgeware import is_dir, file_exists
+import math
+
+class App:
+    def __init__(self, collection, name, path, icon):
+        self.index = len(collection)
+        self.icon = icon
+        self.name = name
+        self.path = path
+        collection.append(self)
+
+    def draw(self, offset, scale=1):
+        # draw the icon sprite
+
+        position = vec2(screen.width // 2, screen.height // 2)
+        position += offset
+
+        screen.blit(
+            self.icon,
+            rect(
+                position.x - 12 * scale,
+                position.y - 12 * scale,
+                24 * scale,
+                24 * scale
+            )
+        )
+
+
+class Apps:
+    def __init__(self, root):
+        self.apps = []
+        self.active_index = 0
+        self.last_active = 0
+        self.change_time = 0
+        self.direction = -1
+        self.launching = False
+
+        def capitalize(word):
+            if len(word) <= 1:
+                return word
+            return word[0].upper() + word[1:]
+
+        for path in os.listdir(root):
+            name = " ".join([capitalize(word) for word in path.split("_")])
+
+            if is_dir(f"{root}/{path}"):
+                if file_exists(f"{root}/{path}/icon.png"):
+                    App(self.apps, name, path, image.load(f"{root}/{path}/icon.png"))
+
+    @property
+    def active(self):
+        return self.apps[self.active_index]
+
+    def prev(self):
+        self.last_active = self.active_index
+        self.active_index = (self.active_index - 1) % len(self)
+        self.change_time = io.ticks
+        self.direction = -1
+
+    def next(self):
+        self.last_active = self.active_index
+        self.active_index = (self.active_index + 1) % len(self)
+        self.change_time = io.ticks
+        self.direction = 1
+
+    def launch(self):
+        self.change_time = io.ticks
+        self.launching = True
+
+    def draw(self):
+        app = self.apps[self.active_index]
+        prev_app = self.apps[self.last_active]
+        animation_done = False
+
+        time_delta = io.ticks - self.change_time
+
+        if time_delta > 250:
+            animation_done = True
+
+        time_delta = min(time_delta, 250)
+        time_delta = time_delta / 250
+
+        scale = 1
+        if self.launching:
+            scale += time_delta * 4
+            time_delta = 1
+
+        offset = vec2(screen.width, 0)
+        offset *= time_delta
+        offset *= self.direction
+
+        prev_app.draw(offset)
+
+        offset.x += -screen.width * self.direction
+        if animation_done:
+            offset.y += math.sin(io.ticks / 150) * 2
+
+        app.draw(offset, scale)
+
+        if self.launching and animation_done:
+            return f"/system/apps/{app.path}"
+
+    def __len__(self):
+        return len(self.apps)
+
+    def __getitem__(self, i):
+        return self.apps[i]
