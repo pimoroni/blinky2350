f2479ae63e295cfd0d07c258b26ff709edb36955
Phil Howard <github@gadgetoid.com>
2026-01-23 10:57:16 +0000

    apps: slow down vector shapes demo.

--- firmware/apps/demos/demos/vector_shapes.py
+++ firmware/apps/demos/demos/vector_shapes.py
@@ -31,12 +31,12 @@ def update():
   
   shape_count = len(shapes)
   
-  a = (io.ticks / 4000) % 1
+  a = (io.ticks / 2000) % 1
 
   alpha_a = math.sin(a * math.pi / 2) * 255
   alpha_b = 255 - alpha_a
   
-  offset = int((io.ticks / 4000) % shape_count)
+  offset = int((io.ticks / 2000) % shape_count)
 
   last_shape = shapes[(offset - 1) % len(shapes)]
   current_shape = shapes[offset]
