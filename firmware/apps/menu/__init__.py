c918ca3a3ccc12d39d747f19c16eda5b96806b8d
thirdr <ryan@pimoroni.com>
2026-02-03 13:06:20 +0000

    apps: battery charging and low warning on menu

--- firmware/apps/menu/__init__.py
+++ firmware/apps/menu/__init__.py
@@ -5,8 +5,9 @@ sys.path.insert(0, "/system/apps/menu")
 sys.path.insert(0, "/")
 os.chdir("/system/apps/menu")
 
-from badgeware import run, set_brightness
+from badgeware import run, set_brightness, is_charging, get_battery_level
 from app import Apps
+import math
 
 
 set_brightness(0.2)
@@ -17,7 +18,27 @@ apps = Apps("/system/apps")
 
 io.poll()
 
+
+def draw_battery(level):
+
+    screen.pen = color.rgb(100, 100, 100)
+    # draw the battery indicator
+    size = (5, 3)
+    pos = (screen.width - (size[0] + 1), 0)
+    screen.shape(shape.rectangle(*pos, *size))
+    screen.put(pos[0] + size[0], pos[1] + 1)
+    screen.pen = color.black
+    screen.shape(shape.rectangle(pos[0] + 1, pos[1] + 1, size[0] - 2, size[1] - 2))
+
+    # draw the battery fill level
+    screen.pen = color.rgb(100, 100, 100)
+    width = ((size[0] - 1) / 100) * level
+    screen.shape(shape.rectangle(pos[0] + 1, pos[1] + 2, width, size[1] - 4))
+
+
+
 def update():
+    global show_battery_level
 
     screen.pen = color.black
     screen.clear()
@@ -32,6 +53,15 @@ def update():
 
     if io.BUTTON_B in io.pressed:
         apps.launch()
+
+    if io.BUTTON_HOME in io.pressed:
+        show_battery_level = io.ticks
+
+    if is_charging():
+        draw_battery((io.ticks / 20) % 100)
+    elif get_battery_level() <=35:
+        if int(math.sin(io.ticks / 250) + 1):
+            draw_battery(0)
 
     # draw menu apps
     return apps.draw()
