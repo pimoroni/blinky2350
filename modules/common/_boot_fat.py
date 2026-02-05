90a93c543bf88ab634436d979f753003e411233b
Phil Howard <github@gadgetoid.com>
2026-02-05 16:19:01 +0000

    CI: Use ffsmake to set filesystem label.

--- modules/common/_boot_fat.py
+++ modules/common/_boot_fat.py
@@ -15,7 +15,7 @@ bdev_lfs = rp2.Flash(start=USER_FLASH_SIZE - LFS_SIZE, len=LFS_SIZE)
 try:
     lfs = os.VfsLfs2(bdev_lfs, progsize=256)
     vfs.mount(lfs, "/")
-    os.listdir("/") # might fail with UnicodeError on corrupt FAT
+    os.listdir("/") # might fail with UnicodeError on corrupt LFS
 except:  # noqa: E722
     os.VfsLfs2.mkfs(bdev_lfs, progsize=256)
     lfs = os.VfsLfs2(bdev_lfs, progsize=256)
@@ -24,38 +24,10 @@ except:  # noqa: E722
 bdev = rp2.Flash(start=0, len=USER_FLASH_SIZE - LFS_SIZE)
 try:
     fat = vfs.VfsFat(bdev)
-    fat.label(FS_LABEL)
     vfs.mount(fat, "/system", readonly=True)
-    os.listdir("/system") # might fail with UnicodeError on corrupt FAT
 
 except:  # noqa: E722
-    import rp2
-    from badgeware import fatal_error
+    pass
 
-    CACHE_FILE = "/.fsbackup"
-    CACHE_SIZE = 16 * 1024
-    BLOCK_SIZE = 4096
-
-    USER_FLASH_SIZE = rp2.Flash().ioctl(4, 0) * rp2.Flash().ioctl(5, 0)
-    LFS_SIZE = 1024 * 1024  # 1MB root filesystem
-
-    buffer = bytearray(BLOCK_SIZE)
-
-
-    bdev = rp2.Flash(start=0, len=USER_FLASH_SIZE - LFS_SIZE)
-
-    try:
-        with open(CACHE_FILE, "rb") as f:
-            for block in range(CACHE_SIZE / BLOCK_SIZE):
-                f.readinto(buffer)
-                bdev.writeblocks(block, buffer)
-        fatal_error("System Error!", "Unable to mount filesystem. Restored FAT from backup!")
-
-    except OSError:
-        fatal_error("System Error!", "Unable to mount filesystem. Uh, not much I can do here sorry! Please re-flash your board.")
-    # vfs.VfsFat.mkfs(bdev)
-    # fat = vfs.VfsFat(bdev)
-    # fat.label(FS_LABEL)
-    # vfs.mount(fat, "/system", readonly=True)
 
 del os, vfs, bdev, bdev_lfs, fat, lfs
